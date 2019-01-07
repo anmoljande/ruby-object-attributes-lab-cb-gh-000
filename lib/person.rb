614b39cfbd6da51c2b9b9d35cc9105852fe216e5
Anmol Jande <jandanmol@gmail.com>
2019-01-07 03:07:40 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,31 @@
-def Person
+class Person
   
+  def name
+    @name 
+  end 
+  def name=(n)
+    @name=n 
+  end 
   
+   def job=(j)
+     @job=j 
+   end 
+   def job
+     @job 
+   end 
+   
 end 
+  def name
+    @name 
+  end 
+  def name=(n)
+    @name=n 
+  end 
+  
+   def job=(j)
+     @job=j 
+   end 
+   def job
+     @job 
+   end 
+   
